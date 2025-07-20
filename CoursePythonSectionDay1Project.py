0bc1a686310949de900f5d090d1758e364510328
Advait more <advaitmore031@gmail.com>
2025-07-20 16:42:18 +0530

    Add files via upload
    Hello!!, This is my very first Python project I made. I am sharing it here to showcase my journey in Python programming.

---
+++ CoursePythonSectionDay1Project.py
@@ -0,0 +1,6 @@
+#Band Name Generator Project.
+print("Hellew~~, welcome to the 🎸band name🤘genetor website!!!!")
+city_Name = input("Enter the name of your City:\n")
+pet_Name = input("Enter the name of your pet:\n")
+name_generator = (city_Name +" "+ pet_Name) 
+print(name_generator)
